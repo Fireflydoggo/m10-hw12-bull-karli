5cc6945ea993afd5481bb4897ffe84d3e863f4b1
Fireflydoggo <bull.karli@gmail.com>
2021-12-07 12:01:57 -0800

    added methods and variable

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // YOUR CODE HERE
+//Your assignment is to create a class named Car in JavaScript. This class will create objects that have three properties:
 class Car {
+  constructor(make, model, year) {
+      this.make = make
+      this.model = model
+      this.year = year
+  }
+
+//Your class should also have the following methods:
+  honk() {
+      console.log('BEEP BEEP!')
+  }
 
 }
 
@@ -7,4 +18,15 @@ setTimeout(function() {
     console.log('maintenance complete')
 }, 3000)
 
-)
+
+
+
+var mySweetRide = new Car('Pontiac', 'Fiero', 1988) 
+
+
+
+
+//call mySweetRide's honk method once
+mySweetRide.honk()
+//call mySweetRide's performMaintenance method once
+mySweetRide.performMaintenance()
